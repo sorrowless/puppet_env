1dba077b9c0037f2667da1b069d168301e54bb6e
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2014-12-02 21:41:13 +0300

    Add wireshark to packages list.

--- envinstall/manifests/packages.pp
+++ envinstall/manifests/packages.pp
@@ -22,7 +22,7 @@
 #
 class envinstall::packages {
 
- $pkgs = [ 'pulseaudio', 'pulseaudio-alsa', 'paprefs', 'pavucontrol', 'pasystray-git', 'xfce4-volumed', 'gstreamer0.10-good-plugins', 'plank', 'plank-config', 'compton', 'cairo-infinality-ultimate', 'fontconfig-infinality-ultimate', 'freetype2-infinality-ultimate', 'xnviewmp', 'xatk', 'rosa-media-player', 'rygel', 'transmission-gtk', 'transmission-remote-gui-bin', 'doublecmd-gtk2', 'nethogs', 'tmux' ]
+ $pkgs = [ 'pulseaudio', 'pulseaudio-alsa', 'paprefs', 'pavucontrol', 'pasystray-git', 'xfce4-volumed', 'gstreamer0.10-good-plugins', 'plank', 'plank-config', 'compton', 'cairo-infinality-ultimate', 'fontconfig-infinality-ultimate', 'freetype2-infinality-ultimate', 'xnviewmp', 'xatk', 'rosa-media-player', 'rygel', 'transmission-gtk', 'transmission-remote-gui-bin', 'doublecmd-gtk2', 'nethogs', 'tmux', 'wireshark' ]
 
  $notebook_pkgs = [ 'bumblebee' ]
 
